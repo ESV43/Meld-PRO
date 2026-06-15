b46cbb62f0393063503f3c685ccf32d871a6b07f
esv43 <142923756+ESV43@users.noreply.github.com>
2026-06-15 12:50:46 +0530

    please work

--- app/src/main/kotlin/com/metrolist/music/ui/screens/settings/PlayerSettings.kt
+++ app/src/main/kotlin/com/metrolist/music/ui/screens/settings/PlayerSettings.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.foundation.background
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
